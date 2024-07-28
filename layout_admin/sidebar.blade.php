81dbdbc99e96f795e617c4a30b9e727965e1f752
Thanh Hậu <thanhhaukg188@gmail.com>
2024-07-28 14:37:44 +0700

    Create new hyper template

---
+++ layout_admin/sidebar.blade.php
@@ -0,0 +1,149 @@
+<div class="leftside-menu">
+    <div class="button-sm-hover" data-bs-toggle="tooltip" data-bs-placement="right" title="Show Full Sidebar">
+        <i class="uil-check-circle"></i>
+    </div>
+
+    <div class="h-100" id="leftside-menu-container" data-simplebar>
+        <div class="leftbar-user">
+            <img src="#" height="50" width="50"
+                 class="rounded-circle shadow-sm">
+            <span class="leftbar-user-name mt-2">
+                Name
+            </span>
+        </div>
+
+        <ul class="side-nav">
+                <li class="side-nav-title">
+                    Navigation
+                </li>
+
+                <li class="side-nav-item">
+                    <a href="#" class="side-nav-link">
+                        <i class="uil-graph-bar"></i>
+                        <span>
+                        Dashboards
+                    </span>
+                    </a>
+                </li>
+
+                <li class="side-nav-title">
+                    Components
+                </li>
+
+                <li class="side-nav-item">
+                    <a data-bs-toggle="collapse" href="#sidebarColections" aria-expanded="false"
+                       aria-controls="sidebarColections"
+                       class="side-nav-link">
+                        <i class="uil-layer-group"></i>
+                        <span>
+                        Colections
+                    </span>
+                        <span class="menu-arrow"></span>
+                    </a>
+                    <div class="collapse" id="sidebarColections">
+                        <ul class="side-nav-second-level">
+                            <li>
+                                <a href="#">
+                                    <i class="uil-shop"></i>
+                                    Citys
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="uil-store-alt"></i>
+                                    Districts
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="uil-store"></i>
+                                    Wards
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+                <li class="side-nav-item">
+                    <a data-bs-toggle="collapse" href="#sidebarAccounts" aria-expanded="false"
+                       aria-controls="sidebarAccounts"
+                       class="side-nav-link">
+                        <i class="uil-user-circle"></i>
+                        <span>
+                        Accounts
+                    </span>
+                        <span class="menu-arrow"></span>
+                    </a>
+                    <div class="collapse" id="sidebarAccounts">
+                        <ul class="side-nav-second-level">
+                            <li>
+                                <a href="#">
+                                    <i class="dripicons-user font-17"></i>
+                                    Admins
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="dripicons-user-group font-17"></i>
+                                    Staffs
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="uil-users-alt"></i>
+                                    Customers
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+                <li class="side-nav-item">
+                    <a href="#" class="side-nav-link">
+                        <i class="dripicons-store font-17"></i>
+                        <span>
+                        Suppliers
+                    </span>
+                    </a>
+                </li>
+                <li class="side-nav-item">
+                    <a href="#" class="side-nav-link">
+                        <i class="uil-label-alt"></i>
+                        <span>
+                        Brands
+                    </span>
+                    </a>
+                </li>
+                <li class="side-nav-item">
+                    <a href="#" class="side-nav-link">
+                        <i class="uil-apps"></i>
+                        <span>
+                        Categorys
+                    </span>
+                    </a>
+                </li>
+            <li class="side-nav-item">
+                <a href="#" class="side-nav-link">
+                    <i class="uil-cube"></i>
+                    <span>
+                        Products
+                    </span>
+                </a>
+            </li>
+            <li class="side-nav-item">
+                <a href="#" class="side-nav-link">
+                    <i class="uil-truck"></i>
+                    <span>
+                        Shipments
+                    </span>
+                </a>
+            </li>
+            <li class="side-nav-item">
+                <a href="#" class="side-nav-link">
+                    <i class="uil-bill"></i>
+                    <span>
+                        Bills
+                    </span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
